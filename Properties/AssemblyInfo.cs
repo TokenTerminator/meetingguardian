b598ac9838618da584c38ca1c4d761b42a4b33ab
TokenTerminator <britoso@gmail.com>
2026-06-30 11:10:44 -0400

    Upload Properties/AssemblyInfo.cs via Code2Hub

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Meeting Guardian")]
+[assembly: AssemblyDescription("Outlook Meeting Monitor and Reminder Tray App")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Accenture")]
+[assembly: AssemblyProduct("Meeting Guardian")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a8f6a374-325a-4b6a-8758-bdcc48f95f2a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
